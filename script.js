e76ced999ae62e321f9ed66550556e39bc306be1
Saurabh Kirve <>
2023-08-02 20:50:35 +0530

    Minor bug fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /* -------------Hamburger Handler-----------
  */
 burger = document.querySelector('.burger')
-navList = document.querySelector('.navList')
 navbar = document.querySelector('.navbar')
-burger = addEventListener('click', ()=>{
+navList = document.querySelector('.navList')
+
+
+burger.addEventListener('click', ()=>{
 
         navList.classList.toggle('v-class');
         navbar.classList.toggle('navbar');
