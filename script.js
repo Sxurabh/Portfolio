d1215138efd5adff33f14972ad1a692c79b01a42
Saurabh Kirve <>
2023-08-02 19:55:02 +0530

    Removed bg.png file. Added style.css file. Updated html file. Added hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+burger = document.querySelector('.burger')
+navList = document.querySelector('.navList')
+navbar = document.querySelector('.navbar')
+burger = addEventListener('click', ()=>{
+
+        navList.classList.toggle('v-class');
+        navbar.classList.toggle('navbar');
+})
